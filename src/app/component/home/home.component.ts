ea8c40b4b556fee6cef736bf7dd8ae1e56ad515a
SadiBarkinSeber <sadibarkn@gmail.com>
2024-07-21 15:02:04 +0300

    Add Cards

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
 
+  products: string[]= [
+    "",
+    "",
+    "",
+    "",
+    ""
+  ]
+  constructor(){}
+ngOnInit(): void {
+  
+}
 }
